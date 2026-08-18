beaa29ad159e6adcffc34201607ad4880b4a7581
trupti-invezza <trupti.khaire@invezzatechnologies.com>
2026-08-18 09:09:32 +0000

    Favicon added

--- application/views/layout/master.php
+++ application/views/layout/master.php
@@ -5,6 +5,9 @@
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="icon" type="image/x-icon" href="<?php echo URL::base(); ?>assets/favicon.ico">
+
         <title>
             <?php echo isset($title) ? HTML::chars($title) : 'SurveyKiwi'; ?>
         </title>
